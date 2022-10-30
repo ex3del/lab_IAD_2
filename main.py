5b0b152ecf98ab2406327cca1761b9e6845c434e
PankratovEP <enkwi@mail.ru>
2022-10-30 23:16:04 +0300

    PCA

--- main.py
+++ main.py
@@ -4,13 +4,19 @@ import pandas as pd
 from sklearn.datasets import load_iris
 import pingouin as pg
 from scipy.cluster.hierarchy import linkage, dendrogram, distance
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
+
 
 iris = load_iris()
-
-#  'sepal length (cm)' ,  'petal length (cm)', 'petal width (cm)', 'sepal width (cm)'
+# 0 - iris_setosa, 1- iris_versicolor, 2-iris_virginica
+# 'sepal length (cm)' ,  'petal length (cm)', 'petal width (cm)', 'sepal width (cm)'
 iris_pd = pd.DataFrame(data=np.c_[iris['data'], iris['target']], columns=iris['feature_names'] + ['target'])
 
 
+# threeD_plot('sepal length (cm)', 'petal length (cm)', 'petal width (cm)')
 def threeD_plot(x, y, z):
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
@@ -24,13 +30,20 @@ def threeD_plot(x, y, z):
 
 def ierarchy():
     # 'single', 'complete', 'average', 'centroid', 'ward'
-    # 'euclidian', 'cityblock', 'chebyshev', 'cosine'
-    cvetochki = linkage(iris_pd.drop(columns=['sepal width (cm)', 'target']), method='ward', metric='cosine')
+    # 'euclidean', 'cityblock', 'chebyshev', 'cosine'
+    cvetochki = linkage(iris_pd.drop(columns=['target']), method='complete', metric='euclidean')
     dendrogram(cvetochki)
     dendrogram(cvetochki, leaf_rotation=90, leaf_font_size=6)
     plt.show()
-ierarchy()
 
 
-
-# threeD_plot('sepal length (cm)', 'petal length (cm)', 'petal width (cm)')
+features = ['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)']
+iris_pd.loc[[iris_pd['target'] == 0.0],'target'] = 'iris_setosa'
+x = iris_pd.loc[:, features].values
+y = iris_pd.loc[:, ['target']].values
+x = StandardScaler().fit_transform(x) # pd.DataFrame(data=x, columns=features).head())
+pca = PCA(n_components=2)
+prcp_compon = pca.fit_transform(x)
+prcp_df = pd.DataFrame(data = prcp_compon, columns=['PC1', 'PC2'])
+final_df = pd.concat([prcp_df, iris_pd['target']], axis=1)
+print(iris_pd.head())
