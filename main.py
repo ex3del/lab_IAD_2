af6ee71b8ee266f515f264e8aee6ba327c753887
PankratovEP <enkwi@mail.ru>
2022-11-02 22:37:36 +0300

    доделал ириски

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.datasets import load_iris
-import pingouin as pg
-from scipy.cluster.hierarchy import linkage, dendrogram, distance
-from sklearn.model_selection import train_test_split
+from scipy.cluster.hierarchy import linkage, dendrogram
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
-
-
+from sklearn.manifold import TSNE
+from sklearn.cluster import KMeans
 
 iris = load_iris()
 # 0 - iris_setosa, 1- iris_versicolor, 2-iris_virginica
@@ -64,3 +62,52 @@ def pca(tabl):
         plt.show()
 
 
+def tsne(table):
+    table = table.drop(columns=['target'])
+    features = ['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)']
+    X = table.loc[:, features].values
+    cvetochki = TSNE(n_components=2, perplexity=6, early_exaggeration=12, n_iter=5000)
+    X_cvetochki = pd.DataFrame(cvetochki.fit_transform(X))
+    print('Kullback-Leibler divergence after optimization: ', cvetochki.kl_divergence_)
+    print('No. of iterations: ', cvetochki.n_iter_)
+    targets = ['iris-setosa', 'iris-versicolor', 'iris-virginica']
+    colors = ['red', 'green', 'blue']
+    koef = [0, 50, 100, 150]
+    k = 1
+    for targ, col in zip(targets, colors):
+        plt.scatter(X_cvetochki[0][koef[k - 1]: koef[k]], X_cvetochki[1][koef[k - 1]: koef[k]], color=col, label=targ)
+        k += 1
+    plt.grid()
+    plt.legend()
+    plt.xlabel('Dimension 1')
+    plt.ylabel('Dimension 2')
+    plt.title('t-SNE')
+    plt.show()
+
+
+def k_means(table):
+    table = table.drop(columns=['target'])
+    model = KMeans(n_clusters=3)
+    model.fit(table)
+    all_predict = model.predict(table)
+    def histog():
+        plt.hist(all_predict, bins=10)
+        plt.xlabel('Номер класса')
+        plt.ylabel('Предполагаемое кол-во эл-ов')
+        plt.show()
+    return all_predict
+
+
+def six_punk(table):
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    table = table.drop(columns=['sepal width (cm)'])
+    table['target'] = k_means(iris_pd)
+    for i in range(3):
+        x = table.loc[table.target == i, :]
+        ax.scatter(x['sepal length (cm)'], x['petal length (cm)'], x['petal width (cm)'], label='Характеристики цветков')
+    ax.set_xlabel('sepal length (cm)')
+    ax.set_ylabel('petal length (cm)')
+    ax.set_zlabel('petal width (cm)')
+    ax.legend()
+    plt.show()
